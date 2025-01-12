5aac6f14562f4f7c3fc3448d9c71bd72122c747b
Aylon Feraru <ayloni88@gmail.com>
2025-01-12 17:31:18 +0200

    My first change inside my branch... hopefully!

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-print("Hello, World :D ♟️")
+print("Hello, World :D ♟️, this is aylons branch")
 
 import os
 
 print(f"The operating system is: {os.name}")
 
+### This is the program inside my branch.
