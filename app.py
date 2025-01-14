7745267f1f335df540d7b801c6c1468da3487c78
Aylon Feraru <ayloni88@gmail.com>
2025-01-14 23:34:41 +0200

    fixed the message in app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-print("Hello, World :D ♟️, this is aylons branch")
-
 import os
 
+print("Hello, World :D ♟️")
 print(f"The operating system is: {os.name}")
 
-### This is the program inside my branch.
