f9b6b3442f1c910a747aefd2184d352309d23efa
Max-SF1 <ayloni88@gmail.com>
2025-05-27 09:06:42 +0300

    applied fix to .gitignore

--- app.py
+++ app.py
@@ -2,22 +2,29 @@
 this is something called a docstring, pylint really likes those. 
 they enforce good coding practices. The more you know! 
 app.py - Runs object detection on chess images using Ultralytics YOLO and displays results.
+pylint also likes the last line to be empty 
+- i guess it means the file ended deliberately and the programmer didn't get shot in the middle of wri 
 """
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from ultralytics import YOLO
+
+
+print("yo")
 
 # Load a pretrained YOLO11n model
 model = YOLO("yolo11n.pt")
+print("first checkpoint")
 
 # Run inference on 'bus.jpg' with arguments
 res = model.predict(
             "/workspace/image/view-chess-pieces-with-shatter-effect.jpg",
-            save=True,
+            save=False, #WAS TRUE
             imgsz=640,
             conf=0.5,
             )
 
+print("second checkpoint")
 plt.imshow(res[0].plot())
 plt.axis("off")
 plt.tight_layout()
@@ -27,3 +34,4 @@ plt.show(block=True)
 
 
 np.zeros_like(res[0].show())
+print("done")
